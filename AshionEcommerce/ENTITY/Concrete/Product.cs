5beaad634a453eea5d8c4b8e90f6906a7575c74a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-08 17:36:20 +0300

    Entity - Concrete - Product

---
+++ AshionEcommerce/ENTITY/Concrete/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Concrete
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+        public double Price { get; set; }
+
+        //Brand
+        //Reviews
+        //Available Color
+        //Available Size
+        //Promotions
+        //Image
+        //specification
+    }
+}
