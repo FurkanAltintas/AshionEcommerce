e2dae91707acff47b0e5365017cc10d24e1c75ea
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-08 17:36:10 +0300

    Entity - Concrete - BaseEntity

---
+++ AshionEcommerce/ENTITY/Concrete/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Concrete
+{
+    public class BaseEntity
+    {
+        public DateTime CreateDate { get; set; }
+        public bool IsAvailability { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
