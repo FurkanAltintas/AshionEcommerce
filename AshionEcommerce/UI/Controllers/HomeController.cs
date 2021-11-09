a9672fb1a31117d584852c03fd9b855fe0ec9e92
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-09 23:58:14 +0300

    Home Controller

--- AshionEcommerce/UI/Controllers/HomeController.cs
+++ AshionEcommerce/UI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL.Concrete;
+using DAL.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,16 @@ namespace UI.Controllers
 {
     public class HomeController : Controller
     {
+        BrandManager brandManager = new BrandManager(new EfBrandRepository());
         // GET: Home
         public ActionResult Index()
         {
             return View();
+        }
+
+        public PartialViewResult Brand()
+        {
+            return PartialView(brandManager.List());
         }
     }
 }
