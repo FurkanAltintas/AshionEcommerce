a3075ae226b1145c2e1444c62ea134b8813b1321
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-09 23:56:54 +0300

    Generic Service

---
+++ AshionEcommerce/BLL/Abstract/IGenericService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Abstract
+{
+    public interface IGenericService<T>
+    {
+        List<T> List(Expression<Func<T, bool>> filter = null);
+
+        void Add(T p);
+        void Update(T p);
+        void Delete(T p);
+        void Delete(int id);
+
+        T Get(Expression<Func<T, bool>> filter = null);
+        T Get(int id);
+    }
+}
