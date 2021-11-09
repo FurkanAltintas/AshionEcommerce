c141fd44b847b8a90c9d06998b077b2d8b5e3df0
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-10 00:01:46 +0300

    Route Config

--- AshionEcommerce/UI/App_Start/RouteConfig.cs
+++ AshionEcommerce/UI/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace UI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Brand",
+                url: "{Brand}",
+                defaults: new { controller = "Brand", action = "Index", String = "" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
