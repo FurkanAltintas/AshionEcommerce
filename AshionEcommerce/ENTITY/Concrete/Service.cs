e8c93275f50324dbd4fc5e91c9178bde32e2fe18
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:57:46 +0300

    Service

---
+++ AshionEcommerce/ENTITY/Concrete/Service.cs
@@ -0,0 +1,19 @@
+﻿using CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Concrete
+{
+    public class Service : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Icon { get; set; }
+    }
+}
