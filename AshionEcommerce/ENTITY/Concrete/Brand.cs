f7ad32a50484925550064c5055869cdb7ad2a0c2
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:57:36 +0300

    Brand

--- AshionEcommerce/ENTITY/Concrete/Brand.cs
+++ AshionEcommerce/ENTITY/Concrete/Brand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CORE.Entity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ENTITY.Concrete
 {
-    public class Brand
+    public class Brand : IEntity
     {
         [Key]
         public int Id { get; set; }
