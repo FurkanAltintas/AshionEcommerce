1cc385dc4e80ac35b55d7025b549a063a18139b6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:51:34 +0300

    Service Controller

---
+++ AshionEcommerce/UI/Controllers/ServiceController.cs
@@ -0,0 +1,20 @@
+﻿using BLL.Concrete;
+using DAL.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceRepository());
+        // GET: Service
+        public PartialViewResult Index()
+        {
+            return PartialView(serviceManager.List());
+        }
+    }
+}
