a36b93e4542c8a190400847478f38559dca693cf
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:56:30 +0300

    User

---
+++ AshionEcommerce/ENTITY/Concrete/User.cs
@@ -0,0 +1,25 @@
+﻿using CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Concrete
+{
+    public class User : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool Gender { get; set; }
+        public DateTime BirthDay { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
