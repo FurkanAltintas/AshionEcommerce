788541bd77d5443dd7469e2cf2a19c72694ca916
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:56:44 +0300

    ProductSize

---
+++ AshionEcommerce/ENTITY/Concrete/ProductSize.cs
@@ -0,0 +1,23 @@
+﻿using CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Concrete
+{
+    public class ProductSize : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+        public int SizeId { get; set; }
+        [ForeignKey("SizeId")]
+        public virtual Size Size { get; set; }
+    }
+}
