29bba19f6a0fb449b4c7bbe4d96946b62ed775bd
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 01:06:55 +0300

    User Service

---
+++ AshionEcommerce/BLL/Abstract/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Abstract
+{
+    public interface IUserService : IGenericService<User>
+    {
+        bool Log(User p);
+    }
+}
