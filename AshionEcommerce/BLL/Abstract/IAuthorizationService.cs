e3e2121145dd0f299a9a1f3167859d6429bb2a7d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 01:05:48 +0300

    Authorization Service

---
+++ AshionEcommerce/BLL/Abstract/IAuthorizationService.cs
@@ -0,0 +1,16 @@
+﻿using ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Abstract
+{
+    public interface IAuthorizationService
+    {
+        bool UserLog(User p);
+
+        bool EmployeeLog(Employee p);
+    }
+}
