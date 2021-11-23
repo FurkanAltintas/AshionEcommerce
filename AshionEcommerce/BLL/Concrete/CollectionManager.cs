431411cc66569a3a1faff093e710cd9611e26914
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 01:07:28 +0300

    Collection Manager

---
+++ AshionEcommerce/BLL/Concrete/CollectionManager.cs
@@ -0,0 +1,61 @@
+﻿using BLL.Abstract;
+using DAL.Abstract;
+using ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Concrete
+{
+    public class CollectionManager : ICollectionService
+    {
+        ICollectionDal _collectionDal;
+
+        public CollectionManager(ICollectionDal collectionDal)
+        {
+            _collectionDal = collectionDal;
+        }
+
+        public void Add(Collection p)
+        {
+            _collectionDal.Add(p);
+        }
+
+        public void Delete(Collection p)
+        {
+            _collectionDal.Delete(p);
+        }
+
+        public void Delete(int id)
+        {
+            _collectionDal.Delete(null, id);
+        }
+
+        public Collection Get(Expression<Func<Collection, bool>> filter = null)
+        {
+            return filter == null ?
+                _collectionDal.Get() :
+                _collectionDal.Get(filter);
+        }
+
+        public Collection Get(int id)
+        {
+            return _collectionDal.Get(id);
+        }
+
+        public List<Collection> List(Expression<Func<Collection, bool>> filter = null)
+        {
+            return filter == null ?
+                _collectionDal.List() :
+                _collectionDal.List(filter);
+        }
+
+        public void Update(Collection p)
+        {
+            _collectionDal.Update(p);
+        }
+    }
+}
