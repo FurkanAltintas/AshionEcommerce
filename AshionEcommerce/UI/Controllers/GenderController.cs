6a4511ec453073ab02308395a32c5db5bcc13a15
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:53:05 +0300

    Gender Controller

---
+++ AshionEcommerce/UI/Controllers/GenderController.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Concrete;
+using DAL.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class GenderController : Controller
+    {
+        GenderManager genderManager = new GenderManager(new EfGenderRepository());
+
+        // GET: Gender
+        public PartialViewResult Index()
+        {
+            return PartialView(genderManager.List());
+        }
+    }
+}
