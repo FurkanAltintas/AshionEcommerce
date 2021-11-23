20a9e41e792780eb6a73fbfae66534d2bc56b5e3
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 01:03:58 +0300

    IQueryable

---
+++ AshionEcommerce/CORE/DataAccess/IQueryable.cs
@@ -0,0 +1,14 @@
+﻿using CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE.DataAccess
+{
+    public interface IQueryable<T> where T : class, IEntity, new()
+    {
+        IQueryable<T> Table { get; }
+    }
+}
