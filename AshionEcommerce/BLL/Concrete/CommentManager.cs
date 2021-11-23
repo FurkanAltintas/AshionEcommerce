95c6a8e7192e89520e6d3b3ec93702edd1ab2e0a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 01:07:41 +0300

    Comment Manager

---
+++ AshionEcommerce/BLL/Concrete/CommentManager.cs
@@ -0,0 +1,61 @@
+﻿using BLL.Abstract;
+using DAL.Abstract;
+using ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Concrete
+{
+    public class CommentManager : ICommentService
+    {
+        ICommentDal _commentDal;
+
+        public CommentManager(ICommentDal commentDal)
+        {
+            _commentDal = commentDal;
+        }
+
+        public void Add(Comment p)
+        {
+            _commentDal.Add(p);
+        }
+
+        public void Delete(Comment p)
+        {
+            _commentDal.Delete(p);
+        }
+
+        public void Delete(int id)
+        {
+            _commentDal.Delete(null, id);
+        }
+
+        public Comment Get(Expression<Func<Comment, bool>> filter = null)
+        {
+            return filter == null ?
+                _commentDal.Get() :
+                _commentDal.Get(filter);
+        }
+
+        public Comment Get(int id)
+        {
+            return _commentDal.Get(id);
+        }
+
+        public List<Comment> List(Expression<Func<Comment, bool>> filter = null)
+        {
+            return filter == null ?
+                _commentDal.List() :
+                _commentDal.List(filter);
+        }
+
+        public void Update(Comment p)
+        {
+            _commentDal.Update(p);
+        }
+    }
+}
