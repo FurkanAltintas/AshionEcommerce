eff032c74d890af2e80076c1e05fab021d1624a0
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:52:13 +0300

    Collection Controller

---
+++ AshionEcommerce/UI/Controllers/CollectionController.cs
@@ -0,0 +1,20 @@
+﻿using BLL.Concrete;
+using DAL.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class CollectionController : Controller
+    {
+        CollectionManager collectionManager = new CollectionManager(new EfCollectionRepository());
+        // GET: Collection
+        public PartialViewResult Index()
+        {
+            return PartialView(collectionManager.List());
+        }
+    }
+}
