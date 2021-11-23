4b2e31854aaf296c073303a5441c0cc353f2be90
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:51:26 +0300

    Size Controller

---
+++ AshionEcommerce/UI/Controllers/SizeController.cs
@@ -0,0 +1,29 @@
+﻿using BLL.Abstract;
+using BLL.Concrete;
+using DAL.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class SizeController : Controller
+    {
+
+        SizeManager sizeManager = new SizeManager(new EfSizeRepository());
+        ProductSizeManager productSizeManager = new ProductSizeManager(new EfProductSizeRepository());
+
+        // GET: Size
+        public PartialViewResult Index()
+        {
+            return PartialView(sizeManager.List());
+        }
+
+        public PartialViewResult ProductSize(int id)
+        {
+            return PartialView(productSizeManager.Size(id));
+        }
+    }
+}
