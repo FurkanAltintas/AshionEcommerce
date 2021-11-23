882d9a371de3837c606b314e30f496c07cc03d23
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:52:03 +0300

    Color Controller

---
+++ AshionEcommerce/UI/Controllers/ColorController.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Concrete;
+using DAL.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class ColorController : Controller
+    {
+        ColorManager colorManager = new ColorManager(new EfColorRepository());
+
+        // GET: Color
+        public PartialViewResult Index()
+        {
+            return PartialView(colorManager.List());
+        }
+    }
+}
