8dc29e93ca0b2f933e5a3166c6dd2a20b1dd6059
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:52:21 +0300

    Category Controller

---
+++ AshionEcommerce/UI/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BLL.Concrete;
+using DAL.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+
+        // GET: Category
+        public PartialViewResult Index()
+        {
+            var list = categoryManager.List();
+            return PartialView(list);
+        }
+    }
+}
