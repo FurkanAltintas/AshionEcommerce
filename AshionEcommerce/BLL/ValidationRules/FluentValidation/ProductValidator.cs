dd5a9191a1611c6e75cc147e7f30adbf2230269d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 01:05:07 +0300

    Product Validator

---
+++ AshionEcommerce/BLL/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,19 @@
+﻿using ENTITY.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Ad alanını boş bırakamazsınız")
+                              .MaximumLength(500).WithMessage("Ad alanı 500 karakterden fazla olamaz");
+        }
+    }
+}
