54ae12bfa533d18de75979027412c1a715588cb6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 01:04:16 +0300

    EfEntityRepositoryBase

---
+++ AshionEcommerce/CORE/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,86 @@
+﻿using CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public TEntity Add(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var added = context.Entry(entity);
+                added.State = EntityState.Added;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+
+        public void Delete(TEntity entity, int id = 0)
+        {
+            using (var context = new TContext())
+            {
+                if (id != 0)
+                {
+                    var deleted = context.Entry(entity);
+                    deleted.State = EntityState.Deleted;
+                }
+                else
+                {
+                    var key = context.Set<TEntity>().Find(id);
+                    var deleted = context.Entry(key);
+                    deleted.State = EntityState.Deleted;
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().FirstOrDefault()
+                    : context.Set<TEntity>().FirstOrDefault(filter);
+            }
+        }
+
+        public TEntity Get(int id)
+        {
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().Find(id);
+            }
+        }
+
+        public List<TEntity> List(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var updated = context.Entry(entity);
+                updated.State = EntityState.Modified;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+    }
+}
