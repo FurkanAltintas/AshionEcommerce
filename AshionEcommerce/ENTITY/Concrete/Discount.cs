b627b73b88a8246b7c9f3574b472d3a1b9ebdfe8
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:57:58 +0300

    Discount

---
+++ AshionEcommerce/ENTITY/Concrete/Discount.cs
@@ -0,0 +1,21 @@
+﻿using CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Concrete
+{
+    public class Discount : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Percent { get; set; }
+        public string Image { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
