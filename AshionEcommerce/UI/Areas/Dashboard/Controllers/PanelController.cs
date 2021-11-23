986833cb4aeac1337daeaf000a7816606d260433
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:55:17 +0300

    Areas - Dashboard - Panel Controller

---
+++ AshionEcommerce/UI/Areas/Dashboard/Controllers/PanelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Areas.Dashboard.Controllers
+{
+    public class PanelController : Controller
+    {
+        // GET: Dashboard/Panel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
