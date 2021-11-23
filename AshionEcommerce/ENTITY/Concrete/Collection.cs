597db54b985b14d6c2681685cbe221780636233e
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:56:56 +0300

    Collection

---
+++ AshionEcommerce/ENTITY/Concrete/Collection.cs
@@ -0,0 +1,19 @@
+﻿using CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Concrete
+{
+    public class Collection : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Image { get; set; }
+    }
+}
