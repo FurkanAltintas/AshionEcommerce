1b3e37c953d77693879a643e73a077522fac458a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:59:48 +0300

    EntityFramework Size Repository

---
+++ AshionEcommerce/DAL/EntityFramework/EfSizeRepository.cs
@@ -0,0 +1,16 @@
+﻿using CORE.DataAccess.EntityFramework;
+using DAL.Abstract;
+using DAL.Concrete;
+using ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EntityFramework
+{
+    public class EfSizeRepository : EfEntityRepositoryBase<Size,Context>, ISizeDal
+    {
+    }
+}
