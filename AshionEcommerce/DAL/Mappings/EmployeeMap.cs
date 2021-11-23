d6172ed235cdbdfe52e461eb7ecd12b2060e4bc9
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:59:13 +0300

    Mappings - Employee

---
+++ AshionEcommerce/DAL/Mappings/EmployeeMap.cs
@@ -0,0 +1,23 @@
+﻿using ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Mappings
+{
+    public class EmployeeMap : EntityTypeConfiguration<Employee>
+    {
+        public EmployeeMap()
+        {
+            ToTable(@"Employees", @"dbo");
+            HasKey(x => x.Id);
+            HasRequired(x => x.Name);
+            HasRequired(x => x.SurName);
+            HasRequired(x => x.Email);
+            HasRequired(x => x.Password);
+        }
+    }
+}
