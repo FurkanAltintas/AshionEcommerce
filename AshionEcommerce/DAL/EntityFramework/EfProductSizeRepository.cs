2ec4fb62070949ee0a9d81c0032d9b3d6c3b409f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 01:00:12 +0300

    EntityFramework ProductSize Repository

---
+++ AshionEcommerce/DAL/EntityFramework/EfProductSizeRepository.cs
@@ -0,0 +1,23 @@
+﻿using CORE.DataAccess.EntityFramework;
+using DAL.Abstract;
+using DAL.Concrete;
+using ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EntityFramework
+{
+    public class EfProductSizeRepository : EfEntityRepositoryBase<ProductSize,Context>, IProductSizeDal
+    {
+        public List<ProductSize> Size(int id)
+        {
+            using (var c = new Context())
+            {
+                return c.ProductSizes.Include("Size").Where(x => x.ProductId == id).ToList();
+            }
+        }
+    }
+}
