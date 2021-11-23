a982700a83c5e6ff5f72c65b04c877a5fb264abb
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 01:00:19 +0300

    EntityFramework Product Repository

--- AshionEcommerce/DAL/EntityFramework/EfProductRepository.cs
+++ AshionEcommerce/DAL/EntityFramework/EfProductRepository.cs
@@ -1,15 +1,51 @@
-﻿using DAL.Abstract;
-using DAL.Repositories;
+﻿using CORE.DataAccess.EntityFramework;
+using DAL.Abstract;
+using DAL.Concrete;
 using ENTITY.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.EntityFramework
 {
-    public class EfProductRepository : GenericRepository<Product>, IProductDal
+    public class EfProductRepository : EfEntityRepositoryBase<Product, Context>, IProductDal
     {
+        public List<Product> ListBrand()
+        {
+            using (var c = new Context())
+            {
+                var list = c.Products.Include(x => x.Brand).ToList();
+                return list;
+            }
+        }
+
+        public List<Product> ListGender()
+        {
+            using (var c = new Context())
+            {
+                return c.Products.Include("Gender").OrderByDescending(x => x.Id).Take(8).ToList();
+            }
+        }
+
+        public Product GetBrand(int id)
+        {
+            using (var c = new Context())
+            {
+                return c.Products.Include(x => x.Brand).FirstOrDefault(x => x.Id == id);
+            }
+        }
+
+        public Product RelatedProducts(int id)
+        {
+            using (var context = new Context())
+            {
+                return context.Products.Include(x => x.Brand)
+                                       .Include(x => x.Gender)
+                                       .Include(x => x.Kind).FirstOrDefault(x => x.Id == id);
+            }
+        }
     }
 }
