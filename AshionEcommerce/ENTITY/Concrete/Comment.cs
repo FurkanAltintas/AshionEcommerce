57c4a5d195f737fe17f8be16741cc66215ab15b8
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:58:05 +0300

    Comment

---
+++ AshionEcommerce/ENTITY/Concrete/Comment.cs
@@ -0,0 +1,25 @@
+﻿using CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Concrete
+{
+    public class Comment : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
