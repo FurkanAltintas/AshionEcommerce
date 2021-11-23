81a5d707e5762148ed76951c4dbe9e929db3a0f6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 01:01:35 +0300

    Context

--- AshionEcommerce/DAL/Concrete/Context.cs
+++ AshionEcommerce/DAL/Concrete/Context.cs
@@ -11,7 +11,20 @@ namespace DAL.Concrete
     public class Context : DbContext
     {
         public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Collection> Collections { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Employee> Employees { get; set; }
         public DbSet<Gender> Genders { get; set; }
+        public DbSet<Kind> Kinds { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductGender> ProductGenders { get; set; }
+        public DbSet<ProductSize> ProductSizes { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
