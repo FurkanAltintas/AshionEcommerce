f104425ea5827bd0e92447bd776df6ec38561bd8
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 01:07:05 +0300

    Authorization Manager

---
+++ AshionEcommerce/BLL/Concrete/AuthorizationManager.cs
@@ -0,0 +1,35 @@
+﻿using BLL.Abstract;
+using DAL.Abstract;
+using ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Concrete
+{
+    public class AuthorizationManager : IAuthorizationService
+    {
+        IEmployeeDal _employeeDal;
+        IUserDal _userDal;
+
+        public AuthorizationManager(IEmployeeDal employeeDal, IUserDal userDal)
+        {
+            _employeeDal = employeeDal;
+            _userDal = userDal;
+        }
+
+        public bool EmployeeLog(Employee p)
+        {
+            var auth = _employeeDal.Get(x => x.Email == p.Email && x.Password == p.Password);
+            return auth == null ? false : true;
+        }
+
+        public bool UserLog(User p)
+        {
+            var auth = _userDal.Get(x => x.Email == p.Email && x.Password == p.Password);
+            return auth == null ? false : true;
+        }
+    }
+}
