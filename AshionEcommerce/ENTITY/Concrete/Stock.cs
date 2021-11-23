5cb5959ec3ed5a5cab25059a30546cd86f62cba1
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:56:35 +0300

    Stock

---
+++ AshionEcommerce/ENTITY/Concrete/Stock.cs
@@ -0,0 +1,19 @@
+﻿using CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Concrete
+{
+    public class Stock : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Total { get; set; }
+
+        public List<Product> Product { get; set; }
+    }
+}
