0742ad198dbbe4ff8fc9d764793208de3335e036
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:57:06 +0300

    Color

---
+++ AshionEcommerce/ENTITY/Concrete/Color.cs
@@ -0,0 +1,19 @@
+﻿using CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Concrete
+{
+    public class Color : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Product> Product { get; set; }
+    }
+}
