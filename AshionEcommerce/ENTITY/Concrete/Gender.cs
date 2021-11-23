21fb914d4bdc1918bd478476fddf59afc7881d6a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:57:51 +0300

    Gender

--- AshionEcommerce/ENTITY/Concrete/Gender.cs
+++ AshionEcommerce/ENTITY/Concrete/Gender.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CORE.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,11 @@ using System.Threading.Tasks;
 
 namespace ENTITY.Concrete
 {
-    public class Gender
+    public class Gender : IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public List<Product> Product { get; set; }
     }
 }
