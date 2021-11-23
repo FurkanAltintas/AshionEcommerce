3e26a000d230c960d025558f28446072c4ceb00d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:59:01 +0300

    Migrations

---
+++ AshionEcommerce/DAL/Migrations/202111131840275_Test.cs
@@ -0,0 +1,28 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Test : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "StockId", c => c.Int(nullable: true));
+            AddColumn("dbo.Products", "ColorId", c => c.Int(nullable: true));
+            CreateIndex("dbo.Products", "StockId");
+            CreateIndex("dbo.Products", "ColorId");
+            AddForeignKey("dbo.Products", "ColorId", "dbo.Colors", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.Products", "StockId", "dbo.Stocks", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "StockId", "dbo.Stocks");
+            DropForeignKey("dbo.Products", "ColorId", "dbo.Colors");
+            DropIndex("dbo.Products", new[] { "ColorId" });
+            DropIndex("dbo.Products", new[] { "StockId" });
+            DropColumn("dbo.Products", "ColorId");
+            DropColumn("dbo.Products", "StockId");
+        }
+    }
+}
