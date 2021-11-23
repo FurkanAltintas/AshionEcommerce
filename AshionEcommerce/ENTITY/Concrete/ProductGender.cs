f707ef65d6834b18bc2d072a4ba35a64fd7b3bf1
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-24 00:58:12 +0300

    ProductGender

---
+++ AshionEcommerce/ENTITY/Concrete/ProductGender.cs
@@ -0,0 +1,23 @@
+﻿using CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Concrete
+{
+    public class ProductGender : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+        public int GenderId { get; set; }
+        [ForeignKey("GenderId")]
+        public virtual Gender Gender { get; set; }
+    }
+}
